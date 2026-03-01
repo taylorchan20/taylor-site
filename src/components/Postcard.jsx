24c015bd635a597015b78880d8e05e438a2963a4
Taylor Chan <chantaylor147@gmail.com>
2026-03-01 14:49:52 -0500

    adjusted sizing to make it fit to mobile

--- src/components/Postcard.jsx
+++ src/components/Postcard.jsx
@@ -14,18 +14,20 @@ import twinkle3Img from '../assets/Twinkle_3.png';
 export default function Postcard() {
   return (
     <div className="postcard">
-      <img src={postcardImg} className = "main-img" alt="Postcard" />
-      <img src={nameImg} className="name" alt="Name" />
-      <img src={logoImg} className="logo" alt="Logo" />
-      <img src={mirrorballImg} className="mirrorball" alt="Mirrorball" />
-      <img src={starsImg} className="stars" alt="Stars" />
-      <img src={shellsImg} className="shells" alt="Shells" />
-      <img src={hatImg} className="hat" alt="Hat" />
-      <img src={diamondImg} className="diamond" alt="Diamond" />
-      <img src={twinkle1Img} className="twinkle1" alt="Twinkle 1" />
-      <img src={twinkle2Img} className="twinkle2" alt="Twinkle 2" />
-      <img src={twinkle3Img} className="twinkle3" alt="Twinkle 3" />
-      <img src={twinkle3Img} className="twinkle4" alt="Twinkle 4" />
+      <div className="postcard-inner">
+        <img src={postcardImg} className="main-img" alt="Postcard" />
+        <img src={nameImg} className="name" alt="Name" />
+        <img src={logoImg} className="logo" alt="Logo" />
+        <img src={mirrorballImg} className="mirrorball" alt="Mirrorball" />
+        <img src={starsImg} className="stars" alt="Stars" />
+        <img src={shellsImg} className="shells" alt="Shells" />
+        <img src={hatImg} className="hat" alt="Hat" />
+        <img src={diamondImg} className="diamond" alt="Diamond" />
+        <img src={twinkle1Img} className="twinkle1" alt="Twinkle 1" />
+        <img src={twinkle2Img} className="twinkle2" alt="Twinkle 2" />
+        <img src={twinkle3Img} className="twinkle3" alt="Twinkle 3" />
+        <img src={twinkle3Img} className="twinkle4" alt="Twinkle 4" />
+      </div>
     </div>
   );
 }
